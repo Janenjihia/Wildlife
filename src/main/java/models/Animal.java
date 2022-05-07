773233d57cb9a5c826c37f0685a0417b9aee438e
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-07 19:14:37 +0300

    Add constructor for properties name,health and age

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Animal {
+    public Animal(String name, String health, String age) {
+        this.name=name;
+        this.health=health;
+        this.age=age;
+
+    }
+
 }
+

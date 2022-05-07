624d64b97e0584837f0035db70a84e7f6cfa8c28
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-07 19:16:57 +0300

    Add test to ascertain animal object instantiates properly

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,7 +1,17 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
+    Animal testAnimal = new Animal("simba", "healthy","newborn");
+
+//Test to confirm animal instantiates correctly
+@Test
+public void animal_instantiatesCorrectly_true() {
+    Animal testAnimal = new Animal("simba", "healthy","newborn");
+    assertEquals(true, testAnimal instanceof Animal);
+}
 
 }

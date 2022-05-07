34e714c3849e6933dd54be1b30649b70c16b48ba
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-07 20:12:10 +0300

    Add database to test class

--- src/test/java/models/EndangaredAnimalTest.java
+++ src/test/java/models/EndangaredAnimalTest.java
@@ -1,7 +1,10 @@
 package models;
-
+import org.junit.Rule;
+import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EndangaredAnimalTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
 }

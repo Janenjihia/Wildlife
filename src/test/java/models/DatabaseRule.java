744fc86ecf4232c0f00af52fba13a33399e1a01a
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-07 19:50:22 +0300

    SET UP DB

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,7 +1,21 @@
 package models;
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DatabaseRule {
+class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","nick","00000000");
+    }
+@Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalQuery = "DELETE FROM animals;";
+            con.createQuery(deleteAnimalQuery).executeUpdate();
 
+        }
+    }
 }

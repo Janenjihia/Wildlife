cfda5b42ac11b175ef85ce1c409d7d3d797b0cb9
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-07 19:22:10 +0300

    Add properties, and getter methods

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,11 +1,27 @@
 package models;
 
+
 public class Animal {
+    private final String name;
+    private final String health;
+    private final String age;
     public Animal(String name, String health, String age) {
         this.name=name;
         this.health=health;
         this.age=age;
 
+    }
+    public String getName() {
+        return name;
+
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
     }
 
 }
--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -13,5 +13,23 @@ public void animal_instantiatesCorrectly_true() {
     Animal testAnimal = new Animal("simba", "healthy","newborn");
     assertEquals(true, testAnimal instanceof Animal);
 }
+    @Test
+    public void getName_animalInstantiatesWithName_simba() {
+        assertEquals("simba", testAnimal.getName());
+    }
+    @Test
+    public void getHealth_animalInstantiatesWithHealth_String() {
+        assertEquals("healthy", testAnimal.getHealth());
+    }
+    @Test
+    public void getAge_animalInstantiatesWithAge_String() {
+        assertEquals("newborn", testAnimal.getAge());
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndHealthAndAgeAreSame_true() {
+        Animal firstAnimal = new Animal("simba", "healthy","newborn");
+        Animal anotherAnimal = new Animal("tinga", "healthy","adult");
+        assertTrue(firstAnimal.equals(anotherAnimal));
+    }
 
 }

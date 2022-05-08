f7a70e7b40d8194fa21cc9a74ff94ad4ad71e9e2
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-08 20:59:51 +0300

    Add Database and ranger properties with the respective method

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,4 +1,14 @@
 package models;
 
+import org.testng.annotations.Test;
+
 public class Ranger {
+
+    private final String name;
+
+    public Ranger(String name) {
+        this.name=name;
+    }
+
+
 }
--- src/test/java/models/RangerTest.java
+++ src/test/java/models/RangerTest.java
@@ -1,7 +1,15 @@
 package models;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Rule;
+import org.junit.Test;
 class RangerTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private Ranger newRanger() {
+        return new Ranger("Mwangi");
+    }
+
 
 }

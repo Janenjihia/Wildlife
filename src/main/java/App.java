2ef91a4143971c4040442174c5df2d201653e60d
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-09 19:40:49 +0300

    Add connection port app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,12 +12,19 @@ public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
 
     }
 
-        //route route
-
-
+        //root route
         get("/",(request, response) -> {
             Map<String, Object> model = new HashMap<>();
             List<Sighting> allSightings = Sighting.all();

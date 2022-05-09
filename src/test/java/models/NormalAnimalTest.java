c25e5b714626a0f491bd2b90c697ac5a444da241
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-09 08:01:59 +0300

    add test for instantiating a new normal animal

--- src/test/java/models/NormalAnimalTest.java
+++ src/test/java/models/NormalAnimalTest.java
@@ -1,10 +1,23 @@
 package models;
+import org.junit.Rule;
 import org.sql2o.Connection;
+import org.testng.annotations.Test;
+
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
-class NormalAnimalTest extends Animal {
+class NormalAnimalTest{
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
+    private NormalAnimal newAnimal() {
+        return new NormalAnimal("Tiger","Healthy","Young");
+    }
+    @Test
+    public void animal_instantiatesCorrectly_true(){
+        NormalAnimal normalAnimal = newAnimal();
+        assertTrue(normalAnimal instanceof NormalAnimal);
+    }
 }
 
 

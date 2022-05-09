de5ec42db2e2a5814e1e1fd42a4841404bc2b2b6
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-09 15:36:50 +0300

    add sucess.hbs to allow submission of new sighting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,12 @@
+import models.EndangeredAnimal;
+import models.NormalAnimal;
+import models.Ranger;
 import models.Sighting;
 import spark.ModelAndView;
-import spark.*;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
-
-import static java.util.Collections.get;
 
 public class App {
 
@@ -51,6 +49,41 @@ public class App {
         Map<String, Object> model = new HashMap<>();
         model.put("sightings", Sighting.all());
         return new ModelAndView(model,"sighting-form.hbs");
+    },new HandlebarsTemplateEngine());
+
+    //Sighting Submission
+    post("/sighting/new",(request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        String rangerName = request.queryParams("rangerName").trim();
+        String animalName = request.queryParams("animalName").trim();
+        String animalAge = request.queryParams("animalAge").trim();
+        String animalHealth = request.queryParams("animalHealth").trim();
+        String location = request.queryParams("location").trim();
+        String animalType = request.queryParams("animalType").trim();
+
+        Ranger newRanger = new Ranger(rangerName);
+        newRanger.save();
+
+        if(animalType.equalsIgnoreCase("Endangered")){
+            EndangeredAnimal endangeredAnimal = new EndangeredAnimal(animalName,animalHealth,animalAge);
+            endangeredAnimal.save();
+            Sighting newSighting = new Sighting(endangeredAnimal.getName(),location,newRanger.getId());
+            newSighting.save();
+        }
+        else{
+            NormalAnimal normalAnimal = new NormalAnimal(animalName,animalHealth,animalAge);
+            normalAnimal.save();
+            Sighting newSighting = new Sighting(normalAnimal.getName(),location,newRanger.getId());
+            newSighting.save();
+        }
+        return new ModelAndView(model,"success.hbs");
+    },new HandlebarsTemplateEngine());
+
+    //get: retrieve all sightings by location
+    get("/sightings",(request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        model.put("sightings", Sighting.all());
+        return new ModelAndView(model,"sighting-locations.hbs");
     },new HandlebarsTemplateEngine());
 
 
